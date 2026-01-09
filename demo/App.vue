fc1c4f0feab20f16df8114ce8ddc7bae12a4f6c1
kmdtmyk <winback_banderas@yahoo.co.jp>
2026-01-09 11:33:08 +0900

    Add demo page

---
+++ demo/App.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+SelectField(
+  :records='records'
+)
+</template>
+
+<script lang='ts' setup>
+import SelectField from '../src/SelectField.vue'
+
+const records = async () => {
+  await sleep(500)
+  return ['a', 'b']
+}
+
+const sleep = (time: number) => {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+</script>
